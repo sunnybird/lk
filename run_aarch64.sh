126a3d1312832b4703e7a6a85d9887c662854d9c
SmartGGB <jinlongfeng@live.com>
2025-04-09 14:57:07 +0800

    Update run_aarch64.sh

--- run_aarch64.sh
+++ run_aarch64.sh
@@ -1,4 +1,5 @@
-export TOOLCHAIN_HOME=$HOME/devtools/armtools/arm-gnu-toolchain-12.2.rel1-x86_64-aarch64-none-elf
+rm -rf build-qemu-virt-*
+export TOOLCHAIN_HOME=$HOME/data/devtools/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-elf
 export PATH=$TOOLCHAIN_HOME/bin:$PATH
 export ARCH_arm64_TOOLCHAIN_PREFIX=aarch64-none-elf-
 ./scripts/do-qemuarm -6 -s 4 -m 16 | tee run_arm64.log
